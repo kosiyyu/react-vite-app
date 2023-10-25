3c8023dc85bbf96536c378f65ae007899d771659
Karol Kos <kosiyyu@gmail.com>
2023-10-25 17:59:46 +0200

    display Tags in Journal

--- src/page/journal/Journal.jsx
+++ src/page/journal/Journal.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react";
 import { JOURNAL_DOWNLOAD_URL } from "../../global"
 import axios from "axios"
+import Tag from "../../components/Tag";
 
 function Journal() {
     const journalId = useParams().id;
@@ -21,19 +22,28 @@ function Journal() {
     return (
         <>
           {journal && (
-            <div>
-              <h1>Journal</h1>
-              <h6><a>:: edit journal ::</a></h6>
-              <ul>
-                <li><strong>Id:</strong> {journal.id}</li>
-                <li><strong>Title 1:</strong> {journal.title1}</li>
-                <li><strong>Issn 1:</strong> {journal.issn1}</li>
-                <li><strong>E-issn 1:</strong> {journal.eissn1}</li>
-                <li><strong>Title 2:</strong> {journal.title2}</li>
-                <li><strong>Issn 2:</strong> {journal.issn2}</li>
-                <li><strong>E-issn 2:</strong> {journal.eissn2}</li>
-              </ul>
-            </div>
+            <>
+                <div>
+                <h1>Journal</h1>
+                <h6><a>:: edit journal ::</a></h6>
+                <ul>
+                    <li><strong>Id:</strong> {journal.id}</li>
+                    <li><strong>Title 1:</strong> {journal.title1}</li>
+                    <li><strong>Issn 1:</strong> {journal.issn1}</li>
+                    <li><strong>E-issn 1:</strong> {journal.eissn1}</li>
+                    <li><strong>Title 2:</strong> {journal.title2}</li>
+                    <li><strong>Issn 2:</strong> {journal.issn2}</li>
+                    <li><strong>E-issn 2:</strong> {journal.eissn2}</li>
+                    <li><strong>Tags:</strong></li>
+                    <div>
+                        {journal.tags.map((tag, index) => (
+                            <Tag tagId={-1} key={index}>{tag.value}</Tag>
+                        ))}
+                    </div>
+                </ul>
+                </div>
+                
+            </>
           )}
         </>
       )
