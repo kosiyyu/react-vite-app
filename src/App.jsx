cce68cbbe580d7a1114213ee681ca82d2aec589b
kosiyyu <kosiyyu@gmail.com>
2023-10-30 22:13:47 +0100

    add Provider to App for Journals

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import Home from './page/Home'
 import Journals from './page/journal/Journals'
 import ValidateJournal from './page/journal/ValidateJournal'
+import SearchTokenProvider from './context/SearchTokenProvider'
 
 function App() {
   return (
@@ -13,8 +14,16 @@ function App() {
       <Routes>
         {/* {main routes} */}
         <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/journals" element={<Journals />}></Route>
-        <Route path="/tags" element={<Journals />}></Route>
+        <Route 
+          path="/journals" 
+          element={
+            <SearchTokenProvider>
+              <Journals />
+            </SearchTokenProvider>
+        }
+          >
+        </Route>
+        <Route path="/tags" element={<></>}></Route>
 
         {/* {sub routes} */}
         <Route path="/journal/:id" element={<ValidateJournal />}></Route>
