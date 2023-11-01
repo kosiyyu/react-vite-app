39f86ad087749487832c3a534bac9116b7543553
kosiyyu <kosiyyu@gmail.com>
2023-11-01 19:34:44 +0100

    reorganize, rename inside Journals

--- src/page/journal/Journals.jsx
+++ src/page/journal/Journals.jsx
@@ -9,80 +9,105 @@ import { SearchTokenContext, defaultSearchToken } from "../../context/SearchToke
 function Journals() {
     const {display, dispatchDisplay, state, dispatch, reset,setReset, setButtonSent } = useContext(SearchTokenContext)
 
-    const [displayModal, setDisplayModal] = useState(false)
-    const [displaySearch, setDisplaySearch] = useState(false)
+    const [isAddJournalModal, setIsAddJournalModal] = useState(false)
+    const [isSearch, setIsSearch] = useState(false)
 
+    // FOR RESET
     const [orderByArgument, setOrderByArgument] = useState("Id")
     const [isDescSort, setDescSort] = useState(false)
-
-    const isMount = useIsMount()
+    const isMountReset = useIsMount()
 
     useEffect(() => {
-        if(isMount)
+        if(isMountReset)
             return
         console.log("JOURNALS RESET")
         setOrderByArgument("Id")
         setDescSort(false)
     }, [reset])
 
-    function updateOnSubmit(e) {
+    // HANDLE FUNCTIONS
+
+    function handleSubmit(e) {
         e.preventDefault()
         dispatch({type: "UPDATE", value: {searchToken: state}})
         dispatchDisplay({type: "UPDATE", value: {searchToken: state}})
         setButtonSent(s => !s)
     }
 
-    function resetToggle(){
+    function handleReset(){
         dispatch({type: "RESET", value: {searchToken: defaultSearchToken}})
         dispatchDisplay({type: "RESET", value: {searchToken: defaultSearchToken}})
         setReset(r => !r)
         
     }
 
+    // DISPLAY FUNCTIONS
+
+    function displayAddJournalModal(){
+        if(isAddJournalModal)
+            return <AddJournalModal closeModal={() => setIsAddJournalModal(false)} />
+        return ""
+    }
+
+    function displaySearchSwitch(){
+        return(
+        <label>
+            <input 
+                type="checkbox" 
+                role="switch" 
+                checked={isSearch ? "active" : ""} 
+                onChange={() => setIsSearch(!isSearch)}
+            />
+            Search filter
+        </label>
+        )
+    }
+
+    function displaySearch(){
+        if(isSearch)
+            return(
+                <form onSubmit={(e) => handleSubmit(e)}>
+                <label>Sort by</label>
+                <select 
+                    value={orderByArgument}
+                    onChange={(e) => setOrderByArgument(e.target.value)}
+                    name="orderBy" 
+                    required
+                >
+                    <option>Id</option>
+                    <option>Title 1</option>
+                    <option>issn1</option>
+                    <option>E-issn 1</option>
+                    <option>Title 2</option>
+                    <option>Issn 2</option>
+                    <option>E-issn 2</option>
+                    <option>Points</option>
+                </select>
+                <JournalSearch></JournalSearch>
+                <TagSearch/>
+                <fieldset>
+                    <label>
+                        Descending order
+                    </label>
+                    <input checked={isDescSort} onChange={(e) => setDescSort(e.target.checked)} type="checkbox" name="isDesc" role="switch"/>
+                </fieldset>
+                <button type="submit">Search</button>
+                <a onClick={handleReset}>Reset your search filters here...</a>
+            </form>
+        )
+        return ""
+    }
+
+    //
 
     return (
         <>
             <h1>📰 Journals</h1>
-            <h6><a onClick={() => setDisplayModal(true)}>:: add journal ::</a></h6>
-            {displayModal && <AddJournalModal closeModal={() => setDisplayModal(false)} />}
-            <label>
-                <input type="checkbox" role="switch" checked={displaySearch ? "active" : ""} onChange={() => setDisplaySearch(!displaySearch)}/>
-                Search filter
-            </label>
-            {displaySearch ? 
-            <div>
-                <form onSubmit={(e) => updateOnSubmit(e)}>
-                    <label>Sort by</label>
-                    <select 
-                        value={orderByArgument}
-                        onChange={(e) => setOrderByArgument(e.target.value)}
-                        name="orderBy" 
-                        required
-                    >
-                        <option>Id</option>
-                        <option>Title 1</option>
-                        <option>issn1</option>
-                        <option>E-issn 1</option>
-                        <option>Title 2</option>
-                        <option>Issn 2</option>
-                        <option>E-issn 2</option>
-                        <option>Points</option>
-                    </select>
-                    <JournalSearch></JournalSearch>
-                    <TagSearch/>
-                    <fieldset>
-                        <label>
-                            Descending order
-                        </label>
-                        <input checked={isDescSort} onChange={(e) => setDescSort(e.target.checked)} type="checkbox" name="isDesc" role="switch"/>
-                    </fieldset>
-                    <button type="submit">Search</button>
-                    <a onClick={resetToggle}>Reset your search filters here...</a>
-                </form>
-            </div>
-            :
-            ""}
-                <JournalList></JournalList>
+            <h6><a onClick={() => setIsAddJournalModal(true)}>:: add journal ::</a></h6>
+            {displayAddJournalModal()}
+            {displaySearchSwitch()}
+            {displaySearch()}
+            <JournalList></JournalList>
         </>
     )
 }
