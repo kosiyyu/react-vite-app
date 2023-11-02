1953ef6c7e86a0f5f82c24665e4bc95927ddbc6c
kosiyyu <kosiyyu@gmail.com>
2023-11-02 14:18:04 +0100

    Update Journal and ValidateJournal to match their tag equivalents

--- src/page/journal/Journal.jsx
+++ src/page/journal/Journal.jsx
@@ -1,50 +1,44 @@
-import { useParams } from "react-router-dom"
-import { useEffect, useState } from "react";
-import { JOURNAL_DOWNLOAD_URL } from "../../global"
-import axios from "axios"
 import Tag from "../../components/Tag";
+import PropTypes from "prop-types"
 
-function Journal() {
-    const journalId = useParams().id;
-    const [journal, setJournals] = useState()
-    
-    useEffect(()=>{
-        axios.get(JOURNAL_DOWNLOAD_URL(journalId))
-            .then(response => {
-                console.log(response.data)
-                setJournals(response.data)
-            })
-            .catch(error => {
-                console.log(`error: ${error}`)
-            })
-    }, [journalId])
+Journal.propTypes = {
+    journalData: PropTypes.object.isRequired
+}
+function Journal(props) {
+    const journal = props.journalData
+
+    function displayTags(){
+        if(journal.tags.length > 0)
+            return (
+                <>
+                    {journal.tags.map((tag, index) => (
+                        <Tag tagId={-1} key={index}>{tag.value}</Tag>
+                    ))}
+                </>
+            )
+        else return "-"
+    }
+
+    function displayJournal(){
+        return (
+            <ul>
+                <li><strong>Id:</strong> {journal.id}</li>
+                <li><strong>Title 1:</strong> {journal.title1}</li>
+                <li><strong>Issn 1:</strong> {journal.issn1}</li>
+                <li><strong>E-issn 1:</strong> {journal.eissn1}</li>
+                <li><strong>Title 2:</strong> {journal.title2}</li>
+                <li><strong>Issn 2:</strong> {journal.issn2}</li>
+                <li><strong>E-issn 2:</strong> {journal.eissn2}</li>
+                <li><strong>Tags:</strong> {displayTags()}</li>
+            </ul>
+        )
+    }
 
     return (
         <>
-          {journal && (
-            <>
-                <div>
-                <h1>Journal</h1>
-                <h6><a>:: edit journal ::</a></h6>
-                <ul>
-                    <li><strong>Id:</strong> {journal.id}</li>
-                    <li><strong>Title 1:</strong> {journal.title1}</li>
-                    <li><strong>Issn 1:</strong> {journal.issn1}</li>
-                    <li><strong>E-issn 1:</strong> {journal.eissn1}</li>
-                    <li><strong>Title 2:</strong> {journal.title2}</li>
-                    <li><strong>Issn 2:</strong> {journal.issn2}</li>
-                    <li><strong>E-issn 2:</strong> {journal.eissn2}</li>
-                    <li><strong>Tags:</strong></li>
-                    <div>
-                        {journal.tags.map((tag, index) => (
-                            <Tag tagId={-1} key={index}>{tag.value}</Tag>
-                        ))}
-                    </div>
-                </ul>
-                </div>
-                
-            </>
-          )}
+            <h1>Journal</h1>
+            <h6><a>:: edit journal ::</a></h6>
+            {displayJournal()}
         </>
       )
 }
--- src/page/journal/ValidateJournal.jsx
+++ src/page/journal/ValidateJournal.jsx
@@ -1,14 +1,38 @@
 import { useParams } from "react-router-dom"
 import NotFound from "../error/NotFound"
 import Journal from "./Journal"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { JOURNAL_DOWNLOAD_URL } from "../../global"
 
 function ValidateJournal() {
     const params = useParams()
     const journalId = params.id
-    if (!isNaN(journalId) && journalId > 0) {
-      return <Journal />
+    const [journal, setJournal] = useState(undefined)
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        axios.get(JOURNAL_DOWNLOAD_URL(journalId))
+        .then(response => {
+            if(response.data !== undefined)
+                setJournal(response.data)
+            setIsLoading(false)
+        })
+        .catch(error => {
+            console.log(error)
+            setIsLoading(false)
+        })
+    }, [journalId])
+
+    function display(){
+        if(isLoading)
+            return <>Loading...</>
+        if(journal)
+            return <Journal journalData={journal} />
+        return <NotFound />
     }
-    return <NotFound />
-  }
+
+    return display()
+}
 
 export default ValidateJournal
