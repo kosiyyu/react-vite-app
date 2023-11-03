e5eda7c459beae1f647b2c337beb7eef6a45a712
kosiyyu <kosiyyu@gmail.com>
2023-11-03 01:21:53 +0100

    make the Csv form work

--- src/page/csv/Csv.jsx
+++ src/page/csv/Csv.jsx
@@ -1,8 +1,41 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { CSV_UPLOAD_URL } from "../../global"
+
 function Csv(){
+    const [file, setFile] = useState(undefined)
+
+    useEffect(()=>{
+        console.log(file)
+    }, [file])
 
     function handleSubmit(e){
+        if(!file){
+            console.log("ERROR")
+        }
         e.preventDefault()
-        console.log(e.target.file.files[0])
+        const formData = new FormData()
+        formData.append("csv", file)
+        axios.post(CSV_UPLOAD_URL, formData, {
+            "headers": {
+                "Content-Type": "multipart/form-data"
+            }
+        })
+        .then(response => {
+            console.log(response.data)
+            setFile(undefined)
+            e.target.reset()
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+
+    function handleOnChnage() {
+        return (e) => {
+            e.preventDefault()
+            setFile(e.target.files[0])
+        }
     }
 
     return (
@@ -16,9 +49,14 @@ function Csv(){
                 <form onSubmit={(e) => handleSubmit(e)}>
                     <fieldset>
                         <label>Csv file</label>
-                        <input name = "file" type="file" accept=".csv" />
+                        <input 
+                            name="file" 
+                            type="file"
+                            onChange={handleOnChnage()}
+                            accept=".csv" 
+                        />
                     </fieldset>
-                    <button type="submit" >Send</button>
+                    <button type="submit">Send</button>
                 </form>
             </article>
         </div>
