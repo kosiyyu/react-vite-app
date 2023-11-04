7d1ce0a86a7bba561a56618e07bd3dad30866aaf
kosiyyu <kosiyyu@gmail.com>
2023-11-04 03:01:08 +0100

    move css import back to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-//import "../node_modules/@picocss/pico/css/pico.classless.css"
+import "../node_modules/@picocss/pico/css/pico.css"
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
